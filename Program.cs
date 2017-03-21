8abf7d2b6698cc740b480ea20dd66367e6339a5b
Hishigbayr <H21ook@users.noreply.github.com>
2017-03-20 19:33:39 -0700

    zam dursleh
    zam dursleh

--- Program.cs
+++ Program.cs
@@ -4,138 +4,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeProject
+namespace ConsoleApplication2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DrawDisplay d = new DrawDisplay();
+            d.DrawRoad();
         }
     }
-    public class Snake
+
+    class Car
     {
-     
-        private int LEFT = 1, RIGHT = 2; //togtmoliig ihewchlen tomoor bichne
-        private Point[] body;
-        private int len {get;set;}
-      /*  public int Len
+        void move()
         {
-            get
+            ConsoleKeyInfo keyInfo;
+            while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
             {
-                return this.len;
-            }
-            set
-            {
-                this.len = value;
-            }
-        }*/
-        public Snake()
-        {
-            Point head = new Point(10, 10);
-        }
-
-        public void go(int direction = 0)
-        {
-            turn(direction);
-        }
-
-
-        public void die()
-        {
-            for (int i = 0; i < Level.HEIGHT; i++)
-            {
-                for (int j = 0; j < Level.WIDTH; j++)
+                switch (keyInfo.Key)
                 {
-                    if (i == 0 && body[0].Y != i || i == Level.HEIGHT - 1 && body[0].Y != i || j == 0 && body[0].X != i || j == Level.WIDTH - 1 && body[0].X != i)
-                    {
-                    }
+                    case ConsoleKey.RightArrow:
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        break;
                 }
             }
-        }
-
-        public void eat(Food food)
-        {
-
-        }
-
-        private void grow()
-        {
-        }
-
-        private void turn(int direction)
-        {
 
         }
     }
 
-    public class Food
+    class Road
     {
-        
+
     }
 
-    public class Level
-    {
-        public static int HEIGHT = 25, WIDTH = 80;
-        public static int speed = 300;
-        public static int[,] map = new int[HEIGHT, WIDTH];
-        public Level()
-        {
-            
-        }
+    class Problem
+    { 
+    }
 
-        private void level1()
+    class Level
+    {
+    }
+
+    class DrawDisplay
+    {
+        public void DrawRoad()
         {
-            for (int i = 0; i < HEIGHT; i++)
+            int k = 30;
+            String[,] road = new String[k, 20];
+            for (int i = 0; i < road.GetLength(0); i++)
             {
-                for (int j = 0; j < WIDTH; j++)
-                {
-                    if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1)
+                for (int j = 0; j < road.GetLength(1); j++)
+                    if (i == 0 || i == road.GetLength(0) - 1 || j == 0 || j == road.GetLength(1) - 1)
                     {
-                        map[i, j] = 1; // hana
+                        road[i, j] = "1";
+                        Console.Write(road[i,j]);
+                    }
+                    else if (i == 28 && j == 9)
+                    {
+                        road[i, j] = "0";
+                        Console.Write(road[i, j]);
+                    }
+                    else if (i == 28 && j == 10)
+                    {
+                        road[i, j] = "0";
+                        Console.Write(road[i, j]);
                     }
                     else
-                        map[i, j] = 0; // hooson talbai
-                }
+                    {
+                        road[i, j] = " ";
+                        Console.Write(road[i,j]);
+                    }
+                Console.WriteLine();
             }
-            speed = 300;
         }
-        private void level2()
-        {
-            level1();
-            speed = 250;
-            //nemj hana zurj bolno
-        }
-    }
-
-    public class Display
-    {
-        public void drawSnake(Snake snake)
-        {
-
-        }
-    }
-    public class Point
-    {
-        public int X,Y;
-        public Point(int x,int y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-
-    }
-    public class Game
-    {
-        public Game()
-        {
-        }
-        public void Start()
-        { }
-        public void Resume()
-        { }
-        public void Pause()
-        { }
-        public void End()
-        {}
     }
 }
